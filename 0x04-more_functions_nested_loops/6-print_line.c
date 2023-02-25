d6451e4b9632bbea841a8e250946c13d6df5a270
Ismail Msumagile <izmailderoar@yahoo.com>
2023-02-25 07:57:33 -0800

    straight

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - function that print '_' when condition is met
+ *
+ * @n: function arguments
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; ++i)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
